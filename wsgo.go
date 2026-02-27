e42b3c2284e859484c5090473cb938ed4721f049
pauldin91 <pauldin91@gmail.com>
2026-02-27 10:44:17 +0200

    remove p2p

--- wsgo.go
+++ wsgo.go
@@ -2,10 +2,8 @@ package wsgo
 
 import (
 	"github.com/pauldin91/wsgo/client"
-	"github.com/pauldin91/wsgo/p2p"
 	"github.com/pauldin91/wsgo/server"
 )
 
 type WsClient = client.WsClient
 type WsServer = server.WsServer
-type P2PServer = p2p.P2PServer
