6c648bdd9a7fb822f94fd7932a4ec99b130fab12
pauldin91 <pauldin91@gmail.com>
2026-02-27 10:47:44 +0200

    expose implemented client & server types

--- wsgo.go
+++ wsgo.go
@@ -5,5 +5,11 @@ import (
 	"github.com/pauldin91/wsgo/server"
 )
 
-type WsClient = client.WsClient
+type Server = server.Server
+type TcpServer = server.TcpServer
 type WsServer = server.WsServer
+type QuicServer = server.QuicServer
+type Client = client.Client
+type TcpClient = client.TcpClient
+type WsClient = client.WsClient
+type QuicClient = client.QuicClient
